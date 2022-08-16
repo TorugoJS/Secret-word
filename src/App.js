10efa6499779af9cad6fc4599c7565bb1d45f2c5
TorugoJS <victor.devfrontend@gmail.com>
2022-08-15 21:18:41 -0300

    Secret-word

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import StartScreen from './components/StartScreen';
 
 function App() {
   return (
     <div className="App">
-     <h2>Secret Word</h2>
+     <StartScreen />
     </div>
   );
 }
---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import './StartScreen.css';
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+        <h1>Secret Word</h1>
+        <p>Deseja jogar?</p>
+        <button>Clique para começar!</button>
+    </div>
+  )
+}
+
+export default StartScreen
